44cfa92b7996c0b1cc70ef30410db6468f6177cd
Greg Hunt <freshbrewedweb@gmail.com>
2017-07-13 16:47:21 -0400

    Added auth provider

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -79,9 +79,9 @@ $app->singleton(
 */
 
 $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
-// 
+//
 
 $app->configure('mail');
 
