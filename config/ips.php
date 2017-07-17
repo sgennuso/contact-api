97c7a832d0d2eab65f08459edfc09f4a30fccf8c
Greg Hunt <freshbrewedweb@gmail.com>
2017-07-17 15:06:21 -0400

    Updated IPs

--- config/ips.php
+++ config/ips.php
@@ -2,14 +2,20 @@
 
 return [
 	'whitelist' => [
+		'23.227.38.64', // audicus.myshopify.com
 		'54.172.192.161', // Reverse Proxy
+		'172.30.1.143', // PRIVATE: Reverse Proxy
 		'54.174.252.180', // contact.audicus.com
 		'54.86.45.102', // aht.audicus.com
+		'172.30.1.12', // PRIVATE: aht.audicus.com
 		'54.234.118.203', // aht-stage.audicus.com
+		'172.30.1.52', // PRIVATE: aht-stage.audicus.com
 		'34.207.124.249', // audicus.com
-		'23.227.38.64', // audicus.myshopify.com
+		'172.30.1.213', // PRIVATE: audicus.com
 		'54.210.17.195', // dev.audicus.com
+		'172.30.1.67', // PRIVATE: dev.audicus.com
 		'34.201.117.172', // staging.audicus.com
+		'172.30.1.175', // PRIVATE: staging.audicus.com
 	],
 	'blacklist' => [
 
