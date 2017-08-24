97a32af29210fc965e4a5ed97b4a7d3b42e2926a
Greg Hunt <freshbrewedweb@gmail.com>
2017-08-24 12:00:27 -0400

    Dump ip on unauth request

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -36,7 +36,7 @@ class Authenticate
     public function handle($request, Closure $next, $guard = null)
     {
         if ( !$this->authorizedIp( $request->ip() ) ) {
-            return response('Unauthorized.', 401);
+            return response('Unauthorized. ('.$request->ip().')', 401);
         }
 
         return $next($request);
