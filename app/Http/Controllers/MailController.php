c8ab087379daff39343259f7c7c1cc6d43ed7ce7
Greg Hunt <freshbrewedweb@gmail.com>
2017-08-24 12:33:41 -0400

    Added from/name to mail send

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -29,8 +29,10 @@ class MailController extends Controller
         $this->validate($request, Sender::getSpecialFields());
 
         try {
+
             Mail::send([], [], function ($message) use ($request) {
-                $message->to($request->to)
+                $message->from(config('mail.from.address'), config('mail.from.name'))
+                ->to($request->to)
                 ->subject($request->subject)
                 ->setBody($request->message, ($request->input('html') == "true") ? 'text/html' : NULL);
             });
